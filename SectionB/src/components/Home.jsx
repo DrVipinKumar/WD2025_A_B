f818392330db9e6c295698fcc08e76987115150a
DrVipinKumar <geniusvipin@gmail.com>
2025-10-16 09:26:25 +0530

    updated on 16/10/2025

--- SectionB/src/components/Home.jsx
+++ SectionB/src/components/Home.jsx
@@ -1,39 +1,37 @@
 import { useEffect, useState } from "react"
 
 const Home = () => {
-  const [products, setProducts] = useState([]);
+  const [users, setUsers] = useState([]);
   useEffect(() => {
-    fetch("https://dummyjson.com/products")
+    fetch("http://localhost:5000/users")
       .then(response => response.json())
-      .then(pdata => setProducts(pdata.products))
+      .then(data => setUsers(data))
      
   }, []);
-  const deleteQuotes = (id) => {
-    setProducts(products.filter((item) => item.id != id));
-  }
-  console.log(products);
-  return (
+  console.log(users);
+    return (
       <div className='d-flex flex-column'>
       <h4>Quotes Information</h4>
-      <table className="table table-warning">
-        <thead className="table-dark">
-          <tr><th>ID</th><th>Product Info</th><th>Image</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {products.map(item => (
-        <tr key={item.id}>
-              <td>{item.id}</td>
-              <td>{item.title}</td>
-              <td><img src={item.images[0]} width={200}></img></td>
-              <td><button
-                onClick={() => deleteQuotes(item.id)} className="btn btn-danger">Delete</button></td>
-        </tr>
-       
-      ))}
-          </tbody>
-        </table>
+      <div className="row d-flex justify-content-center">
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+        {users.map(item => (
+              <tr key={item.id}>
+            <th>{item.id}</th>
+            <th>{item.name}</th>
+            <th>{item.email}</th>
+            </tr>
+        ))}   
+            </tbody>
+          </table>
+      </div>  
       </div>
   )
 }
